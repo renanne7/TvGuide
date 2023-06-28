fcef878a0dd4b772e2068ff977532a6abe699c2c
Rene Salih <46291804+renanne7@users.noreply.github.com>
2023-06-28 17:59:09 +0100

    Add files via upload

--- JsonItems.cs
+++ JsonItems.cs
@@ -1,15 +1,27 @@
-using System.Text.Json.Nodes;
-
 namespace TvGuide
 {
-    class JsonItems
+    internal class TvShowSearchResults
     {
-        public string ?Name { get; set; }
-        public string ?Summary { get; set; }
-        public JsonNode ?AvgRuntime { get; set; }
-        public string ?WebChannel { get; set; }
-        public JsonNode ?Runtime { get; set; }
-        public JsonNode ?ScheduleTime { get; set; }
-        public string ?ScheduleDays { get; set; }
+        public string ?Name { get; init; }
+        public string ?Summary { get; init; }
+        public string ?averageRuntime { get; init; }
+        public WebChannel ?WebChannel { get; init; }
+        public Show ?Show { get; init; }
+    }
+    public class Show
+    {
+        public string ?Name { get; init; }  
+        public string ?Summary { get; init; }
+        public string ?averageRuntime { get; init; }
+        public WebChannel ?WebChannel { get; init; }
+        public Schedule ?Schedule { get; init; }
+    }
+        public class WebChannel
+    {
+        public string ?Name { get; init; }  
+    }
+    public class Schedule
+    {
+        public string ?Time { get; init; }  
     }
 }
